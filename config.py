945a738cccbaeb06caff331185b0e6f785117ae6
Alejandro Almeida <85710982+AAlmeidaM@users.noreply.github.com>
2025-10-31 15:32:03 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "change_me")
+    SECRET_KEY = os.getenv("SECRET_KEY", "1234")
     ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "admin")
-    ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "admin")
+    ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "cbld")
     TIMEZONE = os.getenv("TIMEZONE", "Europe/Madrid")
     NO_SHOW_WIN_POINTS = int(os.getenv("NO_SHOW_WIN_POINTS", "3"))
     PORT = int(os.getenv("PORT", "5000"))
